783c46744c62163979faf31f481e6e0f5b70008b
Cks <pikacusheng@gmail.com>
2017-06-14 02:03:13 +0800

    Pause v1.0 Pause Menu and Function added.

---
+++ Assets/SCRIPTS/GameControllerScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControllerScript : MonoBehaviour {
+	public bool paused;
+	private Sprite menu;
+	private GameObject camera;
+	private Mecha mechaScript;
+	private Shooting shootScript;
+	private Sync_Attack sAttackScript;
+
+	void Start()
+	{
+		paused = false;
+		mechaScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Mecha>();
+		shootScript = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<Shooting>();
+		sAttackScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Sync_Attack>();
+		camera = Camera.main.transform.Find("PauseMenu").gameObject;
+		menu = camera.GetComponent<MenuScript>().menu1;
+	}
+
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.P))
+		{
+			paused = !paused;
+			camera.GetComponent<SpriteRenderer>().sprite = menu;
+		}
+		if(paused)
+		{
+			Time.timeScale = 0;
+			camera.SetActive(true);
+			mechaScript.GetComponent<Mecha>().enabled = false;
+			shootScript.GetComponent<Shooting>().enabled = false;
+			sAttackScript.GetComponent<Sync_Attack>().enabled = false;
+		}
+		else if (!paused)
+		{
+			Time.timeScale = 1;
+			camera.SetActive(false);
+			mechaScript.GetComponent<Mecha>().enabled = true;
+			shootScript.GetComponent<Shooting>().enabled = true;
+			sAttackScript.GetComponent<Sync_Attack>().enabled = true;
+		}
+	}
+}
