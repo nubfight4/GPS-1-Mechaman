7dc040fc34110058398b92d51b55e8cf94bd04d4
Kevin Lau <nubfight@gmail.com>
2017-07-11 03:43:35 +0800

    Fixed bugs in tutorial

--- Assets/SCRIPTS/Tutorial_Dummy_Script.cs
+++ Assets/SCRIPTS/Tutorial_Dummy_Script.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tutorial_Dummy_Script : MonoBehaviour {
+public class Tutorial_Dummy_Script : LifeObject {
 
-
+	void Awake () {
+		SetMaxHP (500);
+		SetHP (GetMaxHP ());
+	}
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
