ee29b5f31d84a7d8fbc7432068bfa68584ec0d6a
Kevin Lau <nubfight@gmail.com>
2017-07-11 04:36:43 +0800

    Added Main Menu and Title Menu + Main Menu button sprites

---
+++ Assets/SCRIPTS/Main Menu Scripts/SelectOnInput.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SelectOnInput : MonoBehaviour {
+
+    public EventSystem eventSyst;
+    public GameObject objSelect;
+
+    private bool buttonSelect;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetAxisRaw("Vertical") != 0 && buttonSelect == false)
+        {
+            eventSyst.SetSelectedGameObject(objSelect);
+            buttonSelect = true;
+        }
+	}
+
+    private void OnDisable()
+    {
+        buttonSelect = false;
+    }
+}
