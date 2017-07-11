022bb7283edbeea5492932353b20ece23b6d7204
Ooi Yee Soon <0111509@kdu-online.com>
2017-07-12 05:10:38 +0800

    Main Menu

---
+++ Assets/SCRIPTS/GameManagerScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManagerScript : MonoBehaviour
+{
+	private static GameManagerScript mInstance = null;
+
+	public static GameManagerScript Instance
+	{
+		get
+		{
+			//! singleton implementation for objects that can dynamically be created (does not have reference to hierarchy objects)
+			if(mInstance == null)
+			{
+				GameObject tempObject = GameObject.FindWithTag("GameManager");
+
+				if(tempObject == null)
+				{
+					tempObject = Instantiate(PrefabManagerScript.Instance.gameManagerPrefab, Vector3.zero, Quaternion.identity);
+				}
+				mInstance = tempObject.GetComponent<GameManagerScript>();
+			}
+			return mInstance;
+		}
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/SCRIPTS/PlayLevel1BGM.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayLevel1BGM : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("AFUBAUIBAF");
+		SoundManagerScript.Instance.PlayBGM(AudioClipID.BGM_LEVEL1);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
